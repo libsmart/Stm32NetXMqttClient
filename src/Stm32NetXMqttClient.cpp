6f633eb0d828ae9a00e8b89aac28d8a8107d6ca6
Roland Rusch <roland@rusch.lu>
2024-05-21 19:18:43 +0200

    #1 Implement library and example

--- src/Stm32NetXMqttClient.cpp
+++ src/Stm32NetXMqttClient.cpp
@@ -5,9 +5,15 @@
 
 #include "Stm32NetXMqttClient.hpp"
 
+#include "Address.hpp"
+
 using namespace Stm32NetXMqttClient;
 
 void MqttClient::createNetworkThread() {
+    log(Stm32ItmLogger::LoggerInterface::Severity::INFORMATIONAL)
+            ->println("Stm32NetXMqttClient::MqttClient::createNetworkThread()");
+
+    // Set the stack for the thread
     setStack(NX->getBytePool()->allocate(LIBSMART_STM32NETX_MQTT_CLIENT_THREAD_STACK_SIZE),
              LIBSMART_STM32NETX_MQTT_CLIENT_THREAD_STACK_SIZE);
 
@@ -20,70 +26,149 @@ void MqttClient::mqttThread() {
     log(Stm32ItmLogger::LoggerInterface::Severity::INFORMATIONAL)
             ->println("Stm32NetXMqttClient::MqttClient::mqttThread()");
 
+    Stm32NetX::Address address;
+    address.nxd_ip_version = 4;
+    address.nxd_ip_address.v4 = IP_ADDRESS(10, 82, 2, 198);;
+
     UINT ret = 1;
 
-    log(Stm32ItmLogger::LoggerInterface::Severity::DEBUGGING)
-            ->println("NX->waitForIpInstance()");
     NX->waitForIpInstance();
-    log(Stm32ItmLogger::LoggerInterface::Severity::DEBUGGING)
-            ->println("NX->waitForPacketPool()");
     NX->waitForPacketPool();
 
     for (;;) {
-        log(Stm32ItmLogger::LoggerInterface::Severity::DEBUGGING)
-                ->println("NX->waitForIp()");
         NX->waitForIp();
 
-        // Create MQTT client instance
-        static char client_name[] = "Stm32NetXMqttClient::MqttClient";
-        log(Stm32ItmLogger::LoggerInterface::Severity::DEBUGGING)
-                ->println("nxd_mqtt_client_create()");
-        ret = nxd_mqtt_client_create(this,
-                                     client_name,
-                                     const_cast<CHAR *>(getClientId()), strlen(getClientId()),
-                                     NX->getIpInstance(),
-                                     NX->getPacketPool(),
-                                     NX->getBytePool()->allocate(LIBSMART_STM32NETX_MQTT_CLIENT_STACK_SIZE),
-                                     LIBSMART_STM32NETX_MQTT_CLIENT_STACK_SIZE,
-                                     LIBSMART_STM32NETX_MQTT_CLIENT_THREAD_PRIORITY,
-                                     NX_NULL, 0);
-        if (ret != NXD_MQTT_SUCCESS) {
-            log(Stm32ItmLogger::LoggerInterface::Severity::ERROR)
-                    ->printf("MQTT client create failed. nxd_mqtt_client_create() = 0x%02x\r\n", ret);
-            assert_param(ret == NXD_MQTT_SUCCESS);
-        }
-
-
-        // Run as long as ip address is valid
         for (;;) {
-            if (!NX->isIpSet()) break;
-            tx_thread_sleep(TX_TIMER_TICKS_PER_SECOND);
-        }
+            // Create MQTT client instance
+            ret = create();
+            if (ret != NXD_MQTT_SUCCESS) {
+                break;
+            }
 
+            // disconnectNotifySet(
+                // bounce<MqttClient, decltype(&MqttClient::disconnectCallback), &MqttClient::disconnectCallback>);
+
+            ret = loginSet("testuser", "eZ.1234");
+            if (ret != NXD_MQTT_SUCCESS) {
+                break;
+            }
+
+            ret = connect(&address, NXD_MQTT_PORT, 30, NX_TRUE,
+                          Stm32ThreadX::WaitOption{TX_TIMER_TICKS_PER_SECOND * 10});
+            if (ret != NXD_MQTT_SUCCESS) {
+                break;
+            }
+
+
+            // Run as long as ip address is valid
+            while (NX->isIpSet() && flags.isSet(IS_CONNECTED)) {
+                tx_thread_sleep(TX_TIMER_TICKS_PER_SECOND);
+            }
+            break;
+        }
 
         // Disconnect from the broker.
-        log(Stm32ItmLogger::LoggerInterface::Severity::DEBUGGING)
-                ->println("nxd_mqtt_client_disconnect()");
-        ret = nxd_mqtt_client_disconnect(this);
-        if (ret != NXD_MQTT_SUCCESS) {
-            log(Stm32ItmLogger::LoggerInterface::Severity::ERROR)
-                    ->printf("MQTT client disconnect failed. nxd_mqtt_client_disconnect() = 0x%02x\r\n", ret);
-            assert_param(ret == NXD_MQTT_SUCCESS);
-        }
+        disconnect();
 
 
         // Delete the client instance, release all the resources.
-        log(Stm32ItmLogger::LoggerInterface::Severity::DEBUGGING)
-                ->println("nxd_mqtt_client_delete()");
-        ret = nxd_mqtt_client_delete(this);
-        if (ret != NXD_MQTT_SUCCESS) {
-            log(Stm32ItmLogger::LoggerInterface::Severity::ERROR)
-                    ->printf("MQTT client delete failed. nxd_mqtt_client_delete() = 0x%02x\r\n", ret);
-            assert_param(ret == NXD_MQTT_SUCCESS);
-        }
+        deleteClient();
+
+        // Wait 5 seconds
+        tx_thread_sleep(TX_TIMER_TICKS_PER_SECOND * 5);
     }
 }
 
 const char *MqttClient::getClientId() {
-    return "clientid";
+    return Nameable::getName();
+}
+
+UINT MqttClient::create() {
+    log(Stm32ItmLogger::LoggerInterface::Severity::INFORMATIONAL)
+            ->printf("Stm32NetXMqttClient::MqttClient[%s]::create()\r\n", getClientId());
+
+
+    // Allocate memory for the mqtt client
+    if (clientStackMemory == nullptr) {
+        clientStackMemory = NX->getBytePool()->allocate(LIBSMART_STM32NETX_MQTT_CLIENT_STACK_SIZE);
+    }
+
+    const auto ret = nxd_mqtt_client_create(this,
+                                            const_cast<CHAR *>("mqtt_client"),
+                                            const_cast<CHAR *>(getClientId()), strlen(getClientId()),
+                                            NX->getIpInstance(),
+                                            NX->getPacketPool(),
+                                            clientStackMemory,
+                                            LIBSMART_STM32NETX_MQTT_CLIENT_STACK_SIZE,
+                                            LIBSMART_STM32NETX_MQTT_CLIENT_THREAD_PRIORITY,
+                                            NX_NULL, 0);
+    if (ret != NXD_MQTT_SUCCESS) {
+        log(Stm32ItmLogger::LoggerInterface::Severity::ERROR)
+                ->printf("MQTT client '%s' create failed. nxd_mqtt_client_create() = 0x%02x\r\n",
+                         getClientId(), ret);
+        return ret;
+    }
+    flags.set(IS_CREATED);
+
+    return ret;
+}
+
+UINT MqttClient::disconnect() {
+    log(Stm32ItmLogger::LoggerInterface::Severity::INFORMATIONAL)
+            ->printf("Stm32NetXMqttClient::MqttClient[%s]::disconnect()\r\n", getClientId());
+
+    const auto ret = nxd_mqtt_client_disconnect(this);
+    if (ret != NXD_MQTT_SUCCESS) {
+        log(Stm32ItmLogger::LoggerInterface::Severity::ERROR)
+                ->printf("MQTT client '%s' disconnect failed. nxd_mqtt_client_disconnect() = 0x%02x\r\n",
+                         getClientId(), ret);
+        return ret;
+    }
+    flags.clear(IS_CONNECTED);
+    return ret;
+}
+
+UINT MqttClient::deleteClient() {
+    log(Stm32ItmLogger::LoggerInterface::Severity::INFORMATIONAL)
+            ->printf("Stm32NetXMqttClient::MqttClient[%s]::deleteClient()\r\n", getClientId());
+
+    const auto ret = nxd_mqtt_client_delete(this);
+    if (ret != NXD_MQTT_SUCCESS) {
+        log(Stm32ItmLogger::LoggerInterface::Severity::ERROR)
+                ->printf("MQTT client '%s' delete failed. nxd_mqtt_client_delete() = 0x%02x\r\n",
+                         getClientId(), ret);
+        return ret;
+    }
+    flags.clear(IS_CONNECTED);
+    flags.clear(IS_CREATED);
+    return ret;
+}
+
+UINT MqttClient::loginSet(const char *username, const char *password) {
+    log(Stm32ItmLogger::LoggerInterface::Severity::INFORMATIONAL)
+            ->printf("Stm32NetXMqttClient::MqttClient[%s]::loginSet(%s, ...)\r\n", getClientId(), username);
+
+    const auto ret = nxd_mqtt_client_login_set(this,
+                                               const_cast<CHAR *>(username), strlen(username),
+                                               const_cast<CHAR *>(password), strlen(password));
+    if (ret != NXD_MQTT_SUCCESS) {
+        log(Stm32ItmLogger::LoggerInterface::Severity::ERROR)
+                ->printf("MQTT client '%s' login set failed. nxd_mqtt_client_login_set() = 0x%02x\r\n",
+                         getClientId(), ret);
+    }
+    return ret;
+}
+
+UINT MqttClient::disconnectNotifySet(void (*disconnect_notify)(NXD_MQTT_CLIENT *client_ptr)) {
+    log(Stm32ItmLogger::LoggerInterface::Severity::INFORMATIONAL)
+            ->printf("Stm32NetXMqttClient::MqttClient[%s]::disconnectNotifySet()\r\n", getClientId());
+
+    const auto ret = nxd_mqtt_client_disconnect_notify_set(this, disconnect_notify);
+    if (ret != NXD_MQTT_SUCCESS) {
+        log(Stm32ItmLogger::LoggerInterface::Severity::ERROR)
+                ->printf(
+                    "MQTT client '%s' disconnect notify set failed. nxd_mqtt_client_disconnect_notify_set() = 0x%02x\r\n",
+                    getClientId(), ret);
+    }
+    return ret;
 }
--- src/Stm32NetXMqttClient.hpp
+++ src/Stm32NetXMqttClient.hpp
@@ -13,33 +13,131 @@
 #include "nxd_mqtt_client.h"
 #include "Stm32ItmLogger.hpp"
 #include "Stm32NetX.hpp"
+#include "WaitOption.hpp"
 
 
 namespace Stm32NetXMqttClient {
+    template<class T, class Method, Method m, class... Params>
+    static auto bounce(NXD_MQTT_CLIENT *mqtt_client_ptr, Params... params) ->
+        decltype(((*reinterpret_cast<T *>(mqtt_client_ptr->nxd_mqtt_thread.tx_thread_entry_parameter)).*m)(params...)) {
+        return ((*reinterpret_cast<T *>(mqtt_client_ptr->nxd_mqtt_thread.tx_thread_entry_parameter)).*m)(params...);
+    }
+
+
     class MqttClient;
     inline MqttClient *mqttClient = {};
 
-    class MqttClient : public Stm32ItmLogger::Loggable, public Stm32ThreadX::Thread, public NXD_MQTT_CLIENT {
+    class MqttClient : public Stm32ItmLogger::Loggable, public Stm32Common::Nameable, public Stm32ThreadX::Thread,
+                       public NXD_MQTT_CLIENT {
+        using Flags = enum {
+            NONE = 0,
+            IS_CREATED = 1 << 0,
+            IS_CONNECTED = 1 << 1,
+            THE_END = 1 << 31
+        };
+
     public:
+        ~MqttClient() override {
+            deleteClient();
+        }
+
         explicit MqttClient(Stm32NetX::NetX *nx)
             : MqttClient(nx, &Stm32ItmLogger::emptyLogger) { ; }
 
         MqttClient(Stm32NetX::NetX *nx, Stm32ItmLogger::LoggerInterface *logger)
             : Loggable(logger),
+              Nameable("clientid"),
               Thread(Stm32ThreadX::BOUNCE(MqttClient, mqttThread), reinterpret_cast<ULONG>(this), priority(),
-                     "Stm32NetXMqttClient::MqttClient"),
-              NXD_MQTT_CLIENT(), NX(nx) {
+                     "Stm32NetXMqttClient::MqttClient::mqttThread()"), NXD_MQTT_CLIENT(), NX(nx) {
+            flags.create();
         }
 
         [[noreturn]] void mqttThread();
 
         const char *getClientId();
 
+        /**
+         * @see https://github.com/eclipse-threadx/rtos-docs/blob/main/rtos-docs/netx-duo/netx-duo-mqtt/chapter3.md#nxd_mqtt_client_create
+         */
+        UINT create();
+
+        /**
+         *
+         * @see https://github.com/eclipse-threadx/rtos-docs/blob/main/rtos-docs/netx-duo/netx-duo-mqtt/chapter3.md#nxd_mqtt_client_connect
+         */
+        UINT connect(NXD_ADDRESS *server_ip, UINT server_port, UINT keepalive, UINT clean_session,
+                     Stm32ThreadX::WaitOption waitOption) {
+            log(Stm32ItmLogger::LoggerInterface::Severity::INFORMATIONAL)
+                    ->printf("Stm32NetXMqttClient::MqttClient[%s]::connect()\r\n", getClientId());
+
+            const auto ret = nxd_mqtt_client_connect(this,
+                                                     server_ip,
+                                                     server_port,
+                                                     keepalive,
+                                                     clean_session,
+                                                     waitOption());
+            if (ret != NXD_MQTT_SUCCESS) {
+                log(Stm32ItmLogger::LoggerInterface::Severity::ERROR)
+                        ->printf("MQTT client '%s' connect failed. nxd_mqtt_client_connect() = 0x%02x\r\n",
+                                 getClientId(), ret);
+                return ret;
+            }
+            flags.set(IS_CONNECTED);
+            return ret;
+        }
+
+        /**
+         * @see https://github.com/eclipse-threadx/rtos-docs/blob/main/rtos-docs/netx-duo/netx-duo-mqtt/chapter3.md#nxd_mqtt_client_disconnect
+         */
+        UINT disconnect();
+
+        /**
+         * @see https://github.com/eclipse-threadx/rtos-docs/blob/main/rtos-docs/netx-duo/netx-duo-mqtt/chapter3.md#nxd_mqtt_client_delete
+         */
+        UINT deleteClient();
+
+        /**
+         * @brief Sets the login credentials for the MQTT client.
+         *
+         * This method sets the login credentials (username and password) for the MQTT client.
+         *
+         * @param username The username for the MQTT client.
+         * @param password The password for the MQTT client.
+         * @return The status code indicating the success or failure of the operation.
+         *
+         * @see https://github.com/eclipse-threadx/rtos-docs/blob/main/rtos-docs/netx-duo/netx-duo-mqtt/chapter3.md#nxd_mqtt_client_set_login
+         */
+        UINT loginSet(const char *username, const char *password);
+
+
+        /**
+         * @see https://github.com/eclipse-threadx/rtos-docs/blob/main/rtos-docs/netx-duo/netx-duo-mqtt/chapter3.md#nxd_mqtt_client_disconnect_notify_set
+         */
+        UINT disconnectNotifySet(VOID (*disconnect_notify)(NXD_MQTT_CLIENT *client_ptr));
+
+
+        static VOID disconnectNotify(NXD_MQTT_CLIENT *client_ptr) {
+            auto self = reinterpret_cast<MqttClient *>(client_ptr);
+            Stm32ItmLogger::logger.printf("disconnectNotify client_ptr=0x%08x\r\n", client_ptr);
+            Stm32ItmLogger::logger.printf("disconnectNotify self=0x%08x\r\n", self);
+        }
+
+        VOID disconnectCallback() {
+            log(Stm32ItmLogger::LoggerInterface::Severity::INFORMATIONAL)
+                    ->printf("Stm32NetXMqttClient::MqttClient[%s]::disconnectCallback()\r\n", getClientId());
+            // Stm32ItmLogger::logger.setSeverity(Stm32ItmLogger::LoggerInterface::Severity::INFORMATIONAL)
+                    // ->println("Stm32NetXMqttClient::MqttClient::disconnectCallback()");
+            // Stm32ItmLogger::logger.printf("disconnectNotify this=0x%08x\r\n", this);
+            flags.clear(IS_CONNECTED);
+        }
+
     protected:
         void createNetworkThread();
 
     private:
         Stm32NetX::NetX *NX;
+        UCHAR *clientStackMemory = {};
+        Stm32ThreadX::EventFlags flags{"Stm32NetXMqttClient::MqttClient::flags", getLogger()};
 
     public:
         static UINT setup(TX_BYTE_POOL *byte_pool) {
